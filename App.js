85103bdd9ded01e28e4807a4448e6eb497f9df25
EmaSuriano <emanuel.suriano@gmail.com>
2019-05-31 10:33:54 +0200

    Improving initial Menu

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Ema from './Ema';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js start working on your app!</Text>
-        <Ema />
-      </View>
-    );
-  }
-}
+import { StyleSheet, ScrollView, Text, View } from 'react-native';
+import { Font } from 'expo';
+import * as Icons from './src/Icon';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginVertical: 40,
+  },
+  icon: {
+    marginVertical: 10,
+    display: 'flex',
     justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+  },
+  iconName: {
+    marginTop: 10,
+    fontFamily: 'roboto-mono',
+    color: 'grey',
   },
 });
+
+export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'roboto-mono': require('./assets/font/RobotoMono-Regular.ttf'),
+      'custom-font-icon': require('./assets/font/custom-font-icon.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
+  render() {
+    if (!this.state.fontLoaded) {
+      return null;
+    }
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          {Object.entries(Icons).map(([icon, Icon]) => (
+            <View style={styles.icon} key={icon}>
+              <Icon size={80} color="navy" />
+              <Text style={styles.iconName}>{`<${icon} />`}</Text>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    );
+  }
+}
